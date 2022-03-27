2fc599dd75d32d02930ecf3a52d052fd29977892
Prashanth Kumar <80522810+knocBack@users.noreply.github.com>
2022-03-27 10:41:48 +0530

    Add files via upload

---
+++ first one/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up*5,ForceMode.VelocityChange);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<Rigidbody>().AddForce(0, 0, 2, ForceMode.VelocityChange);
+        }
+    }
+
+}
